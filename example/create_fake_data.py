316de149aa028f043ec01aa3e1f793902e10b6e4
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2013-08-29 09:54:39 -0500

    added question info

--- example/create_fake_data.py
+++ example/create_fake_data.py
@@ -26,41 +26,52 @@ agreement = ["Strongly disagree","Disagree","Neutral","Agree","Strongly agree"]
 
 # some sample questions
 question_and_choice_list = [
-    ("Do survey analysis tools make you want to barf?", scale1to10),
-    ("Do you love the Excel blue, pink, ... color scheme?", yesno),
-    ("Survey monkey is sweet", agreement),
-    ("Google forms tells me everything I need to know.", agreement),
-    ("You can't get any better (or cheaper) than a PB&J.", scale1to10),
-    ("The lunch buffet is delicious.", scale1to10),
-    ("Any Chipoltletized food is perfect for take-out.", scale1to10),
-    ("Food trucks meet my criteria for culinary independence.", scale1to10),
-    ("I need a tablecloth and Perrier to properly digest lunch.", scale1to10),
-    # ("Gender", ["Male", "Female"]),
-    # ("Age", ["<20", "extremely old"]),
-    # ("Salary", ["$10/hr", "$30k/yr", "much more than that"]),
+    
+    # type, text, choices
+    ("outcome", "Do survey analysis tools make you want to barf?", scale1to10),
+    ("outcome", "Do you love the Excel blue, pink, ... color scheme?", yesno),
+    ("outcome", "Survey monkey is sweet", agreement),
+    ("outcome", "Google forms tells me everything I need to know.", agreement),
+    ("outcome", "You can't get any better (or cheaper) than a PB&J.", scale1to10),
+    ("outcome", "The lunch buffet is delicious.", scale1to10),
+    ("outcome", "Any Chipoltletized food is perfect for take-out.", scale1to10),
+    ("outcome", "Food trucks meet my criteria for culinary independence.", scale1to10),
+    ("outcome", "I need a tablecloth and Perrier to properly digest lunch.", scale1to10),
+    ("demographic", "Gender", ["Male", "Female"]),
+    ("demographic", "Age", ["<20", "extremely old"]),
+    ("demographic", "Salary", ["$10/hr", "$30k/yr", "much more than that"]),
 ]
+
+questions = []
+for q in question_and_choice_list:
+    questions.append({"type":q[0], "text": q[1], "choices": q[2]})
 
 # generate a bunch of fake data, with 'odd' people having a slight
 # preference for lower choices and 'even' people having a slight
 # preference for higher choices
-data = []
+response_list = []
 for i in xrange(opts.nice_respondents):
-    responses = {}
-    for q_number, (question, choices) in enumerate(question_and_choice_list):
+    response = {}
+    for q_number, (t,question, choices) in enumerate(question_and_choice_list):
         n = int(0.7*len(choices))
         if i%2:
             choice = random.choice(choices[:n])
         else:
             choice = random.choice(choices[-n:])
-        responses[q_number+1] = choice
-    data.append(responses)
+        response[q_number+1] = choice
+    response_list.append(response)
 
 # these jerks just click on the first thing they see to finish as
 # quickly as possible
 for i in xrange(opts.asshole_respondents):
-    for q_number, (question, choices) in enumerate(question_and_choice_list):
-        responses[q_number+1] = choices[0]
-    data.append(responses)
+    response = {}
+    for q_number, (t,question, choices) in enumerate(question_and_choice_list):
+        response[q_number+1] = choices[0]
+    response_list.append(response)
 
 # write it out all purty in a useful javascript file for this example
-print("var data = %s;" % json.dumps(data))
+data = {
+    "responses": response_list,
+    "questions": questions,
+}
+print("var data_json = %s;" % json.dumps(data))
