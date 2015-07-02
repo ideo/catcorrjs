9050f08a8c1ab3f3a45e4a59205d66ccf29344aa
gabe gaster <gabe@gabegaster.com>
2015-07-02 12:50:34 -0500

    added support for multi questions and more than 32 questions, removing crossfilter dependency (replaced by underscore dependency for the time being). closes #33

--- catcorr.js
+++ catcorr.js
@@ -1,15 +1,130 @@
 (function (exports) {
     catcorr.version = "0.2.0";
 
+    function get_matching_responses(responses) {
+	// this is the intersection of all people matching responses
+	// across all selected dimensions
+	var result = responses;
+	var selected_questions = questions
+	    .filter(function (q) {return q.has_selection()});
+	selected_questions.forEach(function (q, i) {
+	    result = result.filter(function (response) {
+		return q.response_matches_selected_choices(response);
+	    })
+	})
+	return result;
+    }
+
+    function init_groups(questions, responses) {
+	var groups = questions
+	    .map(function(question){
+		var answers = responses
+		    .map(function(r) { return r[question.number]; });
+		var counts = multi_count(answers);
+		return make_group(counts, question);
+	    });
+
+	groups.update = function (responses) {
+ 	    var matching_responses = get_matching_responses(responses);
+
+
+	    groups.forEach(function(group){
+		var answers = matching_responses
+		    .map(function(r){ 
+			return r[group.question.number];})
+		var counts = multi_count(answers);
+		group.all.forEach(function (o, k) {
+		    o.value = counts[k] || 0;
+		})
+	    });
+	}
+	return groups
+    }
+
+    function histogram_matching_responses(responses) {
+	// A
+	// get_histograms function which takes responses and generates a
+	// object question.number: its histogram for those
+	// responses.
+	// question.number : [{key:choice, value: count},...]
+	// question.number : [{choice:"Male", count:20},...]
+	
+	// to initialize catcorr, we'll call
+	// get_histograms(everybody), In particular,
+	// get_matching_responses(responses) should just work when no
+	// dimensions are selected.
+ 	var matching_responses = get_matching_responses(responses);
+	var groups = questions
+	    .map(function(question){
+		var answers = matching_responses
+		    .map(function(r) { return r[question.number]; });
+		var counts = multi_count(answers);
+		return make_group(counts, question);
+	    });
+	return groups;
+    }
+
+    function make_group(counts, question){
+	var out = {"counts":counts};
+	var to_object = function(v,k){return {key:+k,
+					      value:v};}
+	out.all = _.map(out.counts, to_object);
+	out.__all__ = question.__all__;
+	out.top = function(){ 
+	    return d3.max(_.values(this.counts));
+	    // return the top response
+	}
+	out.all.value = function(){
+	    return d3.sum(out.all, function(o){return o.value});
+	}
+	out.question = question;
+	return out;
+    }
+    
+    function has_selection(){
+	// question needs to bind has_selection method
+	// question needs to maintain state of whether or not it has a
+	// selection on it
+	// question needs to remember which choices have been selected
+	return this.selected_choices.length > 0;
+    }
+
+    function response_matches_selected_choices(response){
+	// question needs response_matches_selected_choices method which
+	// looks into that response and sees if it has choices that match
+	// this question's selected choices.
+	var person_choices = response[this.number];
+	var selected = this.selected_choices;
+	if (typeof(person_choices) === "number") {
+	    return _.contains(selected, 
+			      person_choices);
+	} else {
+	    return _.any(person_choices, 
+			 function (person_choice){
+			     return _.contains(selected,
+					       person_choice)
+			 });
+	}
+    }
+
     exports.catcorr = catcorr;
     function catcorr(div_id, data) {
-        var questions = data.questions;
-        var responses = data.responses;
+	// #########################
+	// debugging --global
+	questions = data.questions;
+        responses = data.responses;
 
         // create the label2index lookup for quickly calculating the
         // x-coordinate on survey answers
-        var label2index = {};
+
+	// debugging so this is global
+        label2index = {};
+
         questions.forEach(function (q) {
+	    // add additional functions questions here
+	    q.has_selection = has_selection;
+	    q.selected_choices = [];
+	    q.response_matches_selected_choices = response_matches_selected_choices;
             label2index[q.number] = {};
             q.choices.forEach(function (choice, j) {
                 label2index[q.number][choice] = j;
@@ -18,9 +133,19 @@
 
         // re-cast non-numeric answers into the corresponding number in
         // label2index so that this whole crossfilter bizness works
+
+	// NOTE: This changes the underlying data passed in. In
+	// particular, if some choices are missing from questions,
+	// then those values in responses will get erased.
         responses.forEach(function (r) {
             questions.forEach(function (q) {
-                r[q.number] = label2index[q.number][r[q.number]];
+                var choice = r[q.number];
+		if (typeof(choice) === "string"){
+		    r[q.number] = label2index[q.number][choice];
+		} else {
+		    r[q.number] = choice.map(function(c){
+			return label2index[q.number][c];});
+		}
             });
         });
 
@@ -38,22 +163,44 @@
         // Various formatters.
         var formatNumber = d3.format(",d");
 
+	// q = data.questions[32];
+	// respondents = crossfilter(data.responses);
+	// respondents.groupAll();
+	// respondents.dimension(function(d){return d[q.number]}).group().all()
+
         // Create the crossfilter for the relevant dimensions and groups.
-        var respondents = crossfilter(responses),
-        all = respondents.groupAll();
-        var dimensions = [];
-        var groups = [];
+        // var respondents = crossfilter(responses),
+        // all = respondents.groupAll();
+        catcorr.groups = [];
+
         questions.forEach(function (q, i) {
-            dimensions.push(respondents.dimension(function(d){return d[q.number]}));
-            groups.push(dimensions[i].group());
+	    // console.log(i);
+	    // responses_to_that_question = respondents.dimension(function(d){
+	    // 	return d[q.number]});
+
+	    // SWITCH
+            // dimensions.push(responses_to_that_question);
+            // groups.push(dimensions[i].group());
+
+	    var answers = responses.map(function(r){
+		return r[q.number]});
+	    var counts = multi_count(answers);
+	    q.__all__ = _.values(counts);
         });
+	// SWITCH
+	// make the groups for the first time
+	catcorr.groups = init_groups(questions, responses);
+	catcorr.groups.update(responses)
+	
 
         // record the total number of respondents in each group. this is
         // used later to correctly figure out the proportionPath lines
         // below
-        groups.forEach(function (g) {
-            g.__all__ = g.all().map(function (o) {return o.value});
-        });
+
+	// SWITCH
+        // groups.forEach(function (g) {
+        //     g.__all__ = g.all().map(function (o) {return o.value});
+        // });
 
         // create a chart for each dimension
         var xscales = [], xscale;
@@ -90,13 +237,19 @@
             // update the yscale to have the maximal possible domain
             // so that heights (and areas) on each of the charts mean
             // the same thing
+
+	    // #########################
+	    // debugging
+	    // console.log(q,i);
+	    // #########################
+
 	    yscale.domain([0, d3.max([
-		yscale.domain()[1], groups[i].top(1)[0].value
+		yscale.domain()[1], catcorr.groups[i].top(1) // [0].value
 	    ])])
 
             // create the chart
-            chart = barChart(q).dimension(dimensions[i])
-                .group(groups[i])
+            chart = barChart(q)
+                .group(catcorr.groups[i])
                 .x(xscale);
             charts.push(chart);
 
@@ -149,11 +302,20 @@
                 return "catcorr "+d+" bar outcome";
             });
 	legend_svg.select(".all_background.all_bar")
-	    .attr("d", ["M",(legend_width-(bar_width-2*bar_gap))/2,",",10,"v",100,"h",bar_width-2*bar_gap,"v",-100].join(""));
+	    .attr("d", ["M",
+			(legend_width-(bar_width-2*bar_gap))/2,
+			",",10,"v",100,"h",bar_width-2*bar_gap,
+			"v",-100].join(""));
 	legend_svg.select(".foreground.bar")
-	    .attr("d", ["M",(legend_width-(bar_width-2*bar_gap))/2,",",80,"v",30,"h",bar_width-2*bar_gap,"v",-30].join(""));
+	    .attr("d", ["M",
+			(legend_width-(bar_width-2*bar_gap))/2,
+			",",80,"v",30,"h",bar_width-2*bar_gap,
+			"v",-30].join(""));
 	legend_svg.select(".all_proportion.all_bar")
-	    .attr("d", ["M",(legend_width-(bar_width-2*bar_gap))/2,",",40,"h",bar_width-2*bar_gap, "M", legend_width/2,",",15,"v",44].join(""));
+	    .attr("d", ["M",
+			(legend_width-(bar_width-2*bar_gap))/2,
+			",",40,"h",bar_width-2*bar_gap, 
+			"M", legend_width/2,",",15,"v",44].join(""));
 
 	// display all respondents label
 	legend_svg.append("foreignObject")
@@ -223,7 +385,8 @@
 		.style("clear", "both")
 	    var color_legend_svg = legend.insert("svg", "div")
 		.attr("width", legend_width)
-		.attr("height", question_types.length*(swatch_w+swatch_gap)+swatch_gap)
+		.attr("height", 
+		      question_types.length*(swatch_w+swatch_gap)+swatch_gap)
 		.style("margin-bottom", 20)
 		.append("g")
 		.attr("transform", "translate(0,0)");
@@ -254,7 +417,7 @@
 
         // Render the total.
         d3.selectAll("aside.catcorr #total")
-            .text(formatNumber(respondents.size()));
+            .text(formatNumber(responses.length));
 
         renderAll();
 
@@ -266,7 +429,8 @@
         // Whenever the brush moves, re-rendering everything.
         function renderAll() {
             chart.each(render);
-            d3.select("aside.catcorr #active").text(formatNumber(all.value()));
+            d3.select("aside.catcorr #active")
+		.text(formatNumber(catcorr.groups[0].all.value()));
         }
 
         window.filter = function(filters) {
@@ -292,6 +456,8 @@
             dimension,
             group,
             round;
+
+	    the_brush = brush;
 
             function chart(div) {
                 var width = d3.max(x.range()),
@@ -354,7 +520,7 @@
                                 }
                                 return "catcorr "+d+" bar "+question.type;
                             })
-                            .datum(group.all());
+                            .datum(catcorr.groups[id].all);
 
                         g.selectAll(".foreground.bar")
                             .attr("clip-path", "url(#clip-" + id + ")");
@@ -453,28 +619,54 @@
 		// it is an exact solution it avoids the flickering
 		// problem
 		// http://stats.stackexchange.com/a/19142/31771
-		function calc_confidence_intervals() {
-		    var N = d3.sum(group.__all__);
-		    var n = all.value();
+		function calc_p(n_people_who_chose_this, 
+				n_total_responses, 
+				n_choices) {
+		    // in multichoice case, n_total_responses is
+		    // really the number of total checked boxes. We
+		    // probably care more about number of people who
+		    // chose this vs people who didnt -- which in the
+		    // multichoice case is != n_total_responses.
 
+		    var pseudocount = 1;
+		    return ((n_people_who_chose_this + pseudocount) / 
+			    (n_total_responses + pseudocount*n_choices));
+		}
+
+		function calc_confidence_intervals() {
+		    // this is the number of total responses to the
+		    // question. if there's one-person one-vote, then
+		    // N is the number of people. In the multi-choice
+		    // case, though, it's the total number of votes
+		    // cast.
+		    var N = d3.sum(group.__all__);
+
+		    // not sure what this is being used for. TODO
+		    var n = catcorr.groups[0].all.value();
+		    n = group.all.value();
+
+		    console.log("confidence", n, N);
 		    // create an array of the probabilities for each
 		    // group. alpha is the hyperparameter of the
 		    // categorical distribution
 		    // http://en.wikipedia.org/wiki/Categorical_distribution
-		    var p=[], alpha=1;
-		    group.__all__.forEach(function (x) {
-			p.push((x + alpha)/(N + alpha*group.__all__.length));
+		    var p = group.__all__.map(function (x) {
+			return calc_p(x, N, group.__all__.length);
 		    });
 
-		    var confidence_intervals = [], bound;
-		    p.forEach(function (pp) {
-			bound = 1.96*Math.sqrt((pp*(1-pp))/n);
-			confidence_intervals.push([
-			    n * Math.max(pp-bound, 0), 
-			    n * Math.min(pp+bound, 1)
-			]);
-		    });
-		    console.log(confidence_intervals);
+		    var confidence_intervals, bound;
+
+		    var get_bound = function(pp){
+			return 1.96*Math.sqrt((pp*(1-pp))/n)
+		    }
+
+		    confidence_intervals = p.map(function(pp,i){
+			// TODO Think carefully about whether this
+			// should be N or n here
+			return [
+			    n * Math.max(pp - get_bound(pp), 0), 
+			    n * Math.min(pp + get_bound(pp), 1)
+			];})
 		    return confidence_intervals;
 		}
 
@@ -494,17 +686,22 @@
 
                     var path = [],
                     i = -1,
-                    n = groups.length,
-                    g, p, lwr, upr,
-                    a = all.value(),
+                    n_groups = groups.length,
+                    g, prob, expected, lwr, upr,
+                    n_selected = catcorr.groups[0].all.value(),
+		    n_responses = responses.length,
+		    n_choices = group.__all__.length,
 		    confidence_intervals;
-		    if (a!=responses.length) {
+		    if (n_selected!=responses.length) {
 			var confidence_intervals = calc_confidence_intervals()
 		    }
-                    while (++i < n) {
+                    while (++i < n_groups) {
                         g = groups[i];
-                        p = a/responses.length*group.__all__[i];
-                        path.push("M", x(g.key-0.5)+bar_gap, ",", y(p), "h", bar_width-2*bar_gap);
+                        prob = calc_p(group.__all__[i], n_responses, n_choices);
+			expected = n_selected*prob;
+                        path.push("M", x(g.key-0.5)+bar_gap, ",", 
+				  y(expected), 
+				  "h", bar_width-2*bar_gap);
 			if (confidence_intervals) {
 			    lwr = confidence_intervals[i][0];
 			    upr = confidence_intervals[i][1];
@@ -555,20 +752,38 @@
                 g.select("#clip-" + id + " rect")
                     .attr("x", x(extent[0]))
                     .attr("width", x(extent[1]) - x(extent[0]));
+		
+		// this is where we tell a question that it has some
+		// selected options
+		questions[id].selected_choices = extent_to_range(extent);
 
-                dimension.filterRange(extent);
+		// remake the groups with this selection
+		catcorr.groups.update(responses)
+		// SWITCH
+                // dimension.filterRange(extent);
             });
 
             brush.on("brushend.chart", function() {
                 if (brush.empty()) {
-                    var div = d3.select(this.parentNode.parentNode.parentNode);
+                    var div = d3.select(this.parentNode
+					.parentNode
+					.parentNode);
                     div.select(".title a").style("display", "none");
                     div.select("#clip-" + id + " rect")
                         .attr("x", null)
                         .attr("width", "100%");
-                    dimension.filterAll();
+
+		    // this is where we tell a question that it does
+		    // not have any selections
+		    questions[id].selected_choices = [];
+
+		    // remake all the groups with these selections
+		    catcorr.groups.update(responses)
+		    // SWITCH
+                    // dimension.filterAll();
                 }
 
+		// this is for animating the brush selection
 		// inspiration from http://bl.ocks.org/mbostock/6232537
 		else {
 		    if (d3.version < "3.3") return;
@@ -579,7 +794,8 @@
 
 		    // transition the brush to a nice place
 		    var extent0 = brush.extent();
-		    var extent1 = extent0.map(function (v) {return d3.round(v+0.5)-0.5});
+		    var extent1 = extent0.map(function (v) 
+					      {return d3.round(v+0.5)-0.5});
 		    
 		    // if empty when rounded, use floor & ceil instead
 		    if (extent1[0] >= extent1[1]) {
@@ -643,3 +859,28 @@
         }
     };
 })(this)
+
+function extent_to_range(extent){
+    // takes something like [-.5, 2.5] --> [0,1,2]
+    var a = extent[0] + .5 , b = extent[1];
+    return _.range(a, b)
+}
+
+function ravel(iterables){
+    var out = [];
+    iterables.forEach(
+	function(iterable){
+	    iterable.forEach(
+		function(thing){ out.push(thing) })});
+    return out;
+}
+
+function multi_count(answers){
+    // counts all the singletons in a list of lists or in a list
+    if (typeof(answers[0]) === "object"){
+	// answers is a list of lists so ravel it into a long list of singletons
+	answers = ravel(answers);
+    }
+    // count singletons
+    return _.countBy(answers);
+}
